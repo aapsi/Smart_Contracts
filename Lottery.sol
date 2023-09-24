ca3a8b05863ab4c3e45963f591bee64f5c67daeb
aapsi <aapsi0001@gmail.com>
2023-09-24 22:55:54 +0530

    get balance func

--- Lottery.sol
+++ Lottery.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
 contract Lottery {
@@ -10,5 +10,23 @@ contract Lottery {
     constructor() {
         // the person who will be deploying the smart contract is the manager
         manager = msg.sender;
+    }
+        
+    function getBalance() public view returns(uint) {
+        require(manager == msg.sender, "You are not the manager");
+        return address(this).balance;
+    }
+
+    function random() internal view returns(uint) {
+        uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    }
+
+    function pickWinner() view public {
+        require(msg.sender == manager, "You are not the manager");
+        require(players.length >= 3, "Players are less than 3");
+
+        uint r = random();
+        uint index = r % players.length;
+
     }
 }
